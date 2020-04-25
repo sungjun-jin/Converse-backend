e0e28343acc9badc106a8321fef60c9cc38d8999
Sung Jun Jin <Sung Jun Jin>
2020-04-25 16:45:04 +0900

    User gender field CharField 수정

--- account/views.py
+++ account/views.py
@@ -11,22 +11,22 @@ class SignUpView(View):
         data = json.loads(request.body)
         try:
             if User.objects.filter(email = data['email']).exists():
-                return JsonResponse({'Message' : 'USER_ALREADY_EXISTS'}, status=400)
+                return JsonResponse({'Message' : 'USER_ALREADY_EXISTS'}, status = 400)
             hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt())
             User (
-                email = data['email'],
-                password = hashed_password.decode('utf-8'),
-                name = data['name'],
-                phone = data['phone'],
-                birth = data['birth'],
-                gender = data['gender'],
+                email         = data['email'],
+                password      = hashed_password.decode('utf-8'),
+                name          = data['name'],
+                gender        = data['gender'],
+                phone         = data['phone'],
+                birth         = data['birth'],
                 email_confirm = data['email_confirm'],
-                text_confirm = data['text_confirm']
+                text_confirm  = data['text_confirm']
             ).save()
-            return HttpResponse(status=200)
+            return HttpResponse(status = 200)
         except KeyError:
-            return JsonResponse({'Message':'INVALID_KEY'},status=400)
+            return JsonResponse({'Message':'INVALID_KEY'},status = 400)
         except ValidationError:
-            return JsonResponse({'Message': 'VALIDATION_ERROR'}, status=400)
+            return JsonResponse({'Message': 'VALIDATION_ERROR'}, status = 400)
         except AttributeError:
-            return JsonResponse({'Message': 'ATTRIBUTE_ERROR'},status=400)
+            return JsonResponse({'Message': 'ATTRIBUTE_ERROR'},status = 400)
