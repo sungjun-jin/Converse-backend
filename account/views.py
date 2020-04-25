228a0d6050ca82dbe5d4f4607128a1d9dfa9282d
Sung Jun Jin <Sung Jun Jin>
2020-04-25 18:30:52 +0900

    sign in endpoint

--- account/views.py
+++ account/views.py
@@ -1,12 +1,13 @@
 import json,bcrypt,jwt
 
-from django.views import View
+from django.views           import View
 from django.http            import HttpResponse,JsonResponse
 from django.core.exceptions import ValidationError
 from .models                import User
 from converse.settings      import SECRET_KEY
 
 class SignUpView(View):
+
     def post(self,request):
         data = json.loads(request.body)
         try:
@@ -28,3 +29,21 @@ class SignUpView(View):
             return JsonResponse({'Message':'INVALID_KEY'},status = 400)
         except ValidationError:
             return JsonResponse({'Message': 'VALIDATION_ERROR'}, status = 400)
+
+class SignInView(View):
+
+    def post(self,request):
+        data = json.loads(request.body)
+        try:
+            data_email = data['email']
+            data_password = data['password']
+
+            if User.objects.filter(email = data_email).exists():
+                user = User.objects.get(email = data_email)
+
+                if bcrypt.checkpw(data_password.encode('utf-8'), user.password.encode('utf-8')):
+                    access_token = jwt.encode({'user_id':user.id}, SECRET_KEY, algorithm = 'HS256')
+                    return JsonResponse({'access_token':access_token.decode('utf-8')}, status = 200)
+            return JsonResponse({'Message':'INVALID_USER'}, status = 401)
+        except  KeyError:
+            return JsonResponse({'Message':'INVALID_KEY'}, status = 400)
