e28ce01c198783a2ac2a1b4c11f2e1be5daf9a12
Sung Jun Jin <Sung Jun Jin>
2020-04-26 22:28:49 +0900

    is_sneakers field 추가

--- product/models.py
+++ product/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     price         = models.PositiveIntegerField(default = 0)
     gender        = models.CharField(max_length = 45)
     color_name    = models.CharField(max_length = 45)
+    is_sneakers   = models.BooleanField(default = False)
     silhouette    = models.ForeignKey('Silhouette', on_delete = models.SET_NULL, null = True)
     category      = models.ForeignKey('Category', on_delete = models.SET_NULL, null = True)
     group         = models.ForeignKey('Group', on_delete = models.SET_NULL, null = True)
