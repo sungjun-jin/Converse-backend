83cf06252591c099f364e0523e299a400bbf6daf
Sung Jun Jin <Sung Jun Jin>
2020-04-29 16:41:16 +0900

    foundation models 수정

--- foundation/models.py
+++ foundation/models.py
@@ -12,10 +12,10 @@ class MainPage(models.Model):
         db_table = 'main_pages'
 
 class Instagram(models.Model):
-    thumbnail     = models.URLField(max_length = 2000)
-    profile_image = models.URLField(max_length = 2000)
-    user          = models.CharField(max_length = 100)
-    created       = models.CharField(max_length = 100)
+    thumbnail        = models.URLField(max_length = 2000)
+    profile_image    = models.URLField(max_length = 2000)
+    user             = models.CharField(max_length = 100)
+    created_at       = models.CharField(max_length = 100)
 
     class Meta:
         db_table = 'instagrams'
@@ -25,11 +25,17 @@ class Store(models.Model):
     address1   = models.CharField(max_length = 300)
     address2   = models.CharField(max_length = 300)
     city       = models.CharField(max_length = 45)
-    phone      = models.CharField(max_length = 100)
-    longitude  = models.CharField(max_length = 100)
-    latitude   = models.CharField(max_length = 100)
+    phone      = models.CharField(max_length = 20)
+    longitude  = models.DecimalField(max_digits = 20, decimal_places = 15)
+    latitude   = models.DecimalField(max_digits = 20, decimal_places = 15)
     state      = models.CharField(max_length = 30)
-    store_type = models.CharField(max_length = 30)
+    store_type = models.ForeignKey('StoreType', on_delete = models.SET_NULL, null = True)
 
     class Meta:
         db_table = 'stores'
+
+class StoreType(models.Model):
+    name = models.CharField(max_length = 45)
+
+    class Meta:
+        db_table = 'store_types'
